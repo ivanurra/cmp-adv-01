3e2ca0d8fb217f927385f41d4953f482079043c0
ivanurra <ivan.urra.dev@gmail.com>
2021-03-22 15:58:14 +0100

    scoped styles

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -14,16 +14,8 @@ export default {
 };
 </script>
 
-<style>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
+<style scoped>
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
